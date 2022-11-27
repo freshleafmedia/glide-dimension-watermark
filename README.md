89e80add0dd17f69f69e8484899cbb31618b3cb2
Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
2022-11-27 14:30:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Dimension Watermark
+
+Automatically add text to an image showing the dimensions.
+
+
+## Overview
+
+It is common when using placeholder images to show the images dimensions in the center of the image.
+This library is a [Glide](https://github.com/thephpleague/glide) manipulator which will do this and automatically
+adjust text size. 
+
+![500x300](examples/500x300.jpg)
+![100x200](examples/100x200.jpg)
+
+*Photo by [Robert Bye](https://unsplash.com/es/@robertbye) on [Unsplash](https://unsplash.com/s/photos/dorset)*
+
+
+## Installation
+
+```
+composer require freshleafmedia/glide-dimension-watermark
+```
+
+
+## Usage
+
+```php
+use Freshleafmedia\DimensionWatermark\DimensionWatermark;
+
+// Setup Glide server. See https://glide.thephpleague.com/2.0/simple-example/
+$server = League\Glide\ServerFactory::create([
+    // ...
+]);
+
+
+// Add DimensionWatermark to the manipulators array
+$manipulators = $api->getManipulators();
+
+array_splice($manipulators, count($manipulators) - 1, 0, [new DimensionWatermark()]);
+
+$api->setManipulators($manipulators);
+```
+
+Note: It is important that `DimensionWatermark` is added after the `Resize` manipulator and before `Encode` manipulator
+
+
+
+## Tests
+
+Unit tests can be run via `composer test`
+
+
+## License
+
+See [LICENSE](LICENSE)
---
+++ src/DimensionWatermark.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+namespace Freshleafmedia\DimensionWatermark;
+
+use Intervention\Image\AbstractFont;
+use Intervention\Image\Image;
+use League\Glide\Manipulators\BaseManipulator;
+
+class DimensionWatermark extends BaseManipulator
+{
+    public function run(Image $image): Image
+    {
+        for ($offsetX = -2; $offsetX <= 2; $offsetX++) {
+            for ($offsetY = -2; $offsetY <= 2; $offsetY++) {
+                $image->text(
+                    $image->width() . 'x' . $image->height(),
+                    ($image->width() / 2) + $offsetX,
+                    ($image->height() / 2) + $offsetY,
+                    function (AbstractFont $font) use ($image): void {
+                        $font->file(__DIR__ . '/../fonts/OpenSans-Regular-webfont.ttf');
+                        $font->size($this->calculateBestFontSize($image, $font, 60));
+                        $font->align('center');
+                        $font->valign('center');
+                        $font->color('#000');
+                    });
+            }
+        }
+
+        $image->text(
+            $image->width() . 'x' . $image->height(),
+            $image->width() / 2,
+            $image->height() / 2,
+            function (AbstractFont $font) use ($image): void {
+                $font->file(__DIR__ . '/../fonts/OpenSans-Regular-webfont.ttf');
+                $font->size($this->calculateBestFontSize($image, $font, 60));
+                $font->align('center');
+                $font->valign('center');
+                $font->color('#FFF');
+            }
+        );
+
+        return $image;
+    }
+
+    protected function calculateBestFontSize(Image $image, AbstractFont $font, int $max, int $min = 5): int
+    {
+        $paddingPx = 30;
+        $font->size($max);
+
+        while ($font->getBoxSize()['width'] > ($image->width() - $paddingPx) || $font->getBoxSize()['height'] > ($image->height() - $paddingPx)) {
+            $size = $font->getSize() - 5;
+
+            if ($size <= $min) {
+                break;
+            }
+
+            $font->size($size);
+        }
+
+        return $font->size;
+    }
+}
